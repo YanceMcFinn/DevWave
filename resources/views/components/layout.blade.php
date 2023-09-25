cb03e34d06a6c88090e081ca29f3a038b344183c
Sean Cole <seancoledesign@gmail.com>
2023-09-24 20:47:48 -0400

    fixed footer

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -41,7 +41,7 @@
         </script>
         <title>DevWave | Find Developer Jobs & Projects</title>
     </head>
-    <body class="mb-48 bg-black text-white">
+    <body class=" bg-black text-white">
         <nav class="flex justify-between items-center mb-4">
             <a href="/"
                 ><img class="w-24" src="{{asset('images/DevWave_Logo_V1.png')}}" alt="" class="logo"
@@ -82,15 +82,9 @@
             {{$slot}}
         </main>
         <footer
-            class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-devwave-500 text-white h-24 mt-24 opacity-90 md:justify-center"
+            class=" bottom-0 left-0 w-full flex items-center justify-start font-bold bg-devwave-500 text-white h-24 mt-24 opacity-90 md:justify-center"
         >
             <p class="ml-2">Copyright &copy; Sean Cole 2023, All Rights reserved</p>
-
-            <a
-                href="/listings/create"
-                class="absolute top-1/3 right-10 bg-black text-white py-2 px-5"
-                >Post Job</a
-            >
         </footer>
         <x-flash-message />
     </body>
